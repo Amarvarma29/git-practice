11a65fc31eab6b0b24d907746c4a4dca53c31baf
Amar <amarveeru.t999@gmail.com>
2024-12-14 16:20:29 +0530

    variables file

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,12 +5,13 @@ passwd=$2
 
 
 echo "enete username:"
-read  username 
+read -s username 
 # read commmand used for read the username here means take the input 
+# -s used for hiding purpose at run time 
 
 echo "username is : $username"
 echo "enter passwd:"
-read passwd
+read -s passwd
 echo "passwd is : $passwd"
  # above code is for we are giving values to username and passwd
 
