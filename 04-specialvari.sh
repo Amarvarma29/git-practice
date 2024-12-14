c856b71307861d688af5010a6d67826aa27f6c59
Amar <amarveeru.t999@gmail.com>
2024-12-14 17:12:24 +0530

    variables file

---
+++ 04-specialvari.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo "print all the variables in the script : $@ "
+echo "Number of variables passed : $#"
+echo " script name :$0"
+echo "current working directory: $pwd"
+echo "Home directory of the user :$home"
+echo "pid of the script excuting now :$$"
