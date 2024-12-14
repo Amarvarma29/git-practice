f855023992ee9cc15909f2db0aba0fffbc3f7e7e
Amar <amarveeru.t999@gmail.com>
2024-12-14 16:13:45 +0530

    variables file

--- 02-variables.sh
+++ 02-variables.sh
@@ -5,8 +5,8 @@ passwd=$2
 
 echo "enete username:"
 read  username
-echo "username is : $1"
+echo "username is : $username"
 echo "enter passwd:"
 read passwd
-echo "passwd is : $2"
+echo "passwd is : $passwd"
 
