76f0bec7e84af8dfbd22ab458db08472a7c7e8d4
Amar <amarveeru.t999@gmail.com>
2025-02-25 20:45:26 +0530

    commitimg changes in the loop

--- 08-loops.sh
+++ 08-loops.sh
@@ -23,14 +23,14 @@ Check_Root
  
  for package in $@  # $@ referes to all arguments passed to it
  do 
-   dnf list installed $package
-   if [ $? -ne 0 ]
+    dnf list installed $package
+    if [ $? -ne 0 ]
     then 
-       echo "$package is not installed going intall it"
-       dnf install $package -y
-       Validate $? "Installing $package"
+         echo "$package is not installed going intall it"
+         dnf install $package -y
+         Validate $? "Installing $package"
     else 
-       echo "$package isalredy installed  nothinhg to do"
+         echo "$package isalredy installed  nothinhg to do"
     fi 
    
   done
